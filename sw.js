77e408375ec145d63fd1a8265562a951abdd7a10
mahditavakoli7777-ctrl <mahditavakoli7777@gmail.com>
2026-06-17 13:55:23 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'risk-calc-v1';
+const FILES = [
+  './risk-calculator.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
